a9c34e30e97d7df4027a7ad200d3d5605daba32b
AlexDi <mirag0505@yandex.ru>
2023-05-25 16:55:07 +0300

    change localState to redux-toolkit store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useAppSelector, increment, useAppDispatch } from "./counterReducer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const countFromReducer = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -18,8 +20,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          count is {countFromReducer}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
