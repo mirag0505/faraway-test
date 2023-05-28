286fd122d530df1a6534a027f0703c7234a35629
AlexDi <mirag0505@yandex.ru>
2023-05-28 19:57:04 +0300

    added handle error route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-
 import "./App.css";
-import { FC } from "react";
-import { CustomLayout } from "./components/CustomLayout";
-import { Route, Routes } from "react-router-dom";
-import { Editor } from "./components/Editor";
+import {FC} from "react";
+import {CustomLayout} from "./components/CustomLayout";
+import {Route, Routes} from "react-router-dom";
+import {Editor} from "./components/Editor";
 import {HomePage} from "./pages/HomePage.tsx";
+
 export const App: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<CustomLayout />}>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/people/:peopleId" element={<Editor />} />
+        <Route path="/people/:peopleId" element={<Editor/>}/>
+          <Route path="*" element={'Error'}/>
       </Route>
     </Routes>
   );
