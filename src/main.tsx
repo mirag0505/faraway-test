b8d367c7a202c8e441902333a4cf3537484bc248
AlexDi <mirag0505@yandex.ru>
2023-05-28 18:28:24 +0300

    Revert "Updates"
    This reverts commit 9044d248faa039d16b286c7814b21d5fcf0f1a1e.

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import { store } from "./redux/store.ts";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
