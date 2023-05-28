9044d248faa039d16b286c7814b21d5fcf0f1a1e
AlexDi <mirag0505@yandex.ru>
2023-05-28 16:13:50 +0300

    Updates

--- src/utilites/getNumberPersonFromUrl.ts
+++
@@ -1,8 +0,0 @@
-export function getNumberPersonFromUrl(url: string): number {
-  if (Array.isArray(url)) {
-    return 0;
-  }
-
-  const splitedUrl = url.split("/");
-  return Number(splitedUrl.at(-2)) || 0;
-}
